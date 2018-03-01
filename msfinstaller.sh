70172144ff0d7ffa28013183fac4a39bf596d28b
HiddenCoder <31147960+hiddencoder1337@users.noreply.github.com>
2018-03-01 01:31:32 +0000

    Add files via upload

---
+++ msfinstaller.sh
@@ -0,0 +1,91 @@
+#!/data/data/com.termux/files/usr/bin/bash
+echo "
+"
+clear
+echo "       -{ Metaslpoit Framework Installer } "
+echo "            -{ Coded by HiddenCoder } "
+echo ""
+echo ""
+echo "Using And Installing This Tools That Mean You\nReady For"
+echo ""
+echo "[1] Use At Your Own Risk"
+echo "[2] No Warranty"
+echo "[3] If it violates the law with this tool the risk is borne by the user"
+echo ""
+echo "-------------------------------------------"
+echo "Are You Sure Want To Install Metasploit ?"
+echo "-------------------------------------------"
+echo ""
+echo "Press Enter if you Agree / Continue Install"
+echo "Press CTRL + C if you Disagree / Cancel Install"
+read ENTER
+
+echo "####################################"
+apt install autoconf bison clang coreutils curl findutils git apr apr-util libffi-dev libgmp-dev libpcap-dev postgresql-dev readline-dev libsqlite-dev openssl-dev libtool libxml2-dev libxslt-dev ncurses-dev pkg-config postgresql-contrib wget make ruby-dev libgrpc-dev ncurses-utils termux-tools -y
+echo "####################################"
+echo "Downloading & Extracting....."
+
+
+cd $HOME
+curl -LO https://github.com/rapid7/metasploit-framework/archive/4.16.16.tar.gz
+tar -xf $HOME/4.16.16.tar.gz
+mv $HOME/metasploit-framework-4.16.16 $HOME/metasploit-framework
+rm $HOME/4.16.16.tar.gz
+cd $HOME/metasploit-framework
+sed '/rbnacl/d' -i Gemfile.lock
+sed '/rbnacl/d' -i metasploit-framework.gemspec
+gem install bundler
+sed 's|nokogiri (1.*)|nokogiri (1.8.0)|g' -i Gemfile.lock
+
+gem install nokogiri -v 1.8.0 -- --use-system-libraries
+ 
+#sed 's|grpc (.*|grpc (1.4.1)|g' -i $HOME/metasploit-framework/Gemfile.lock
+#gem unpack grpc -v 1.4.1
+#cd grpc-1.4.1
+#curl -LO https://raw.githubusercontent.com/grpc/grpc/v1.4.1/grpc.gemspec
+#curl -L https://raw.githubusercontent.com/Hax4us/Hax4us.github.io/master/extconf.patch
+#patch -p1 < extconf.patch
+#gem build grpc.gemspec
+#gem install grpc-1.4.1.gem
+#cd ..
+#rm -r grpc-1.4.1
+
+
+cd $HOME/metasploit-framework
+bundle install -j5
+$PREFIX/bin/find -type f -executable -exec termux-fix-shebang \{\} \;
+rm ./modules/auxiliary/gather/http_pdf_authors.rb
+if [ -e $PREFIX/bin/msfconsole ];then
+	rm $PREFIX/bin/msfconsole
+fi
+if [ -e $PREFIX/bin/msfvenom ];then
+	rm $PREFIX/bin/msfvenom
+fi
+ln -s $HOME/metasploit-framework/msfconsole /data/data/com.termux/files/usr/bin/
+ln -s $HOME/metasploit-framework/msfvenom /data/data/com.termux/files/usr/bin/
+termux-elf-cleaner /data/data/com.termux/files/usr/lib/ruby/gems/2.4.0/gems/pg-0.20.0/lib/pg_ext.so
+echo "Creating database"
+
+cd $HOME/metasploit-framework/config
+curl -LO https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/database.yml
+
+mkdir -p $PREFIX/var/lib/postgresql
+initdb $PREFIX/var/lib/postgresql
+
+pg_ctl -D $PREFIX/var/lib/postgresql start
+createuser msf
+createdb msf_database
+
+echo ""
+echo "".
+echo "Thanks  To  Termux Indonesia"
+echo ""
+echo "For your support"
+echo "And For Team "
+echo "Eagle Cyber Army "
+echo "
+"
+echo " Contact me : hiddencoder[at]gmail.com "
+echo "
+"
+echo " NOW YOU CAN LAUNCH METASPLOIT BY JUST EXECUTE THE COMMAND :=> msfconsole"
